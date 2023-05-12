25fdb590880e3706d7f7865769fd8138d0e2baea
fiya99banu <92871864+fiya99banu@users.noreply.github.com>
2023-05-12 14:10:25 +0530

    Add files via upload

---
+++ admin/adminslotbadmintondelete.php
@@ -0,0 +1,19 @@
+<?php
+
+//delete.php
+
+if(isset($_POST["id"]))
+{
+ $connect = new PDO('mysql:host=127.0.0.1:3300;dbname=stadia-new', 'root', '');
+ $query = "
+ DELETE from  adminslotbadminton WHERE id=:id
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute(
+  array(
+   ':id' => $_POST['id']
+  )
+ );
+}
+
+?>
