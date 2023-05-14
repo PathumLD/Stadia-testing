f47b999950c2c4f1e335d083a2fb38519b77b02c
fiya99banu <92871864+fiya99banu@users.noreply.github.com>
2023-05-14 12:04:03 +0530

    Add files via upload

--- include/adminnavbar.php
+++ include/adminnavbar.php
@@ -32,7 +32,34 @@
         
     <div class="profile-details">
 
-        <a href='adminnotifications.php'><i style='color:white;' class='fa fa-bell'></i></a>
+       
+
+            <i style='color:white;' class='fa fa-bell'><a href='adminnotifications.php'><?php
+            // Check if the user is logged in
+            if (isset($_SESSION["email"])) {
+                // Get the email of the logged in user
+                $email = $_SESSION["email"];
+
+                // Query the notifications table for the specified email address
+                $query = "SELECT COUNT(*) as count FROM notifications WHERE email = '$email' AND is_read = 0 ";
+                $result = mysqli_query($linkDB, $query);
+
+                // Get the notification count
+                $row = mysqli_fetch_assoc($result);
+                $notification_count = $row["count"];
+
+                // Display the notification count alongside the notification icon
+                echo "<div class='notification-icon' style=\"margin-top: -39px; margin-bottom: -5px; font-weight: bold;\">";
+
+                if ($notification_count > 0) {
+                    echo  "<span class='notification-count' style='color:red;'>$notification_count</span>";
+                }
+                echo "</div>";
+            }
+            ?> </i>
+        </a>
         <img src="../images/profile.jpg" alt="">
         
-      </div>
+        </div>
+
+    
