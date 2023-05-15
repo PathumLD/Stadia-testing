4306baeeef6a6c67ec269af1b047cda1f1c2c2e1
michellenikeetha <88478424+michellenikeetha@users.noreply.github.com>
2023-05-15 08:33:19 +0530

    Add files via upload

--- include/navbar.php
+++ include/navbar.php
@@ -48,10 +48,34 @@
         
     <div class="profile-details">
 
+        <a href='clientnotifications.php'><i style='color:white;' class='fa fa-bell'>
+            <?php
+                // Check if the user is logged in
+                if (isset($_SESSION["email"])) {
+                    // Get the email of the logged in user
+                    $email = $_SESSION["email"];
+
+                    // Query the notifications table for the specified email address
+                    $query = "SELECT COUNT(*) as count FROM notifications WHERE email = '$email' AND is_read = 0 ";
+                    $result = mysqli_query($linkDB, $query);
+
+                    // Get the notification count
+                    $row = mysqli_fetch_assoc($result);
+                    $notification_count = $row["count"];
+
+                    // Display the notification count alongside the notification icon
+                    echo "<div class='notification-icon' style=\"margin-top: -39px;  font-weight: bold;\">";
+
+                    if ($notification_count > 0) {
+                        echo  "<span class='notification-count' style='color:black;'>$notification_count</span>";
+                    }
+                    echo "</div>";
+                }
+                ?> 
+        </i></a>
         <a href='clientcart.php'><i style='color:white;' class='fa fa-shopping-cart' ></i></a>
-        <a href='clientnotifications.php'><i style='color:white;' class='fa fa-bell'></i></a>
         
-        <?php 
+       <?php 
     
     // Retrieve the image from the database
     $folder = "../img/";
@@ -63,7 +87,7 @@
       // Display the image on the web page
       echo '<img src="' . $folder . $filename . '" alt ="dp">';
       } else {
-      echo '<img src="../images/profile.jpg"> ' ;
+      echo '<img src="../img/noprofile.jpg"> ' ;
       }
     
     ?>
